3366e09187b9324f544a85329795d93e5c42fb3f
Melissa Brazle <melissaabrazle@outlook.com>
2023-03-12 13:32:03 -0500

    modified index.js, package.json & added .results.json

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    
+    cats.push("Ralph");
+
+    return cats;
+}
+
+function destructivelyPrependCat(name){
+
+    cats.unshift("Bob");
+
+    return cats;
+}
+
+function destructivelyRemoveLastCat(name){
+
+    cats.pop();
+
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(name){
+
+    cats.shift();
+
+    return cats;
+}
+
+function appendCat(name){
+
+    const appendCat = [...cats, "Broom"];
+    
+    return appendCat;
+}
+
+
+function prependCat(name){
+    const prependCat = ["Arnold",...cats];
+    
+    return prependCat;
+}
+
+function removeLastCat(){
+    const removeLastCat = cats.slice(0,-1); 
+    return removeLastCat;
+}
+
+function removeFirstCat(){
+    const removeFirstCat = cats.slice(1);
+    return removeFirstCat;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
